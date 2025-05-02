0974fea941028b3ce6aad05b1bb5f2e21a7e575d
Mansi-2103 <mansikri2103@gmail.com>
2025-05-02 11:57:03 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Login from "./components/Login";
+
+
 function App(){
-    return<h1>
-      Hello World
-    </h1>
+    return<>
+      < Login/>
+    </>
 }
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+
+
+function Login(){
+    return(
+        <>
+        <section className="min-h-screen flex items-center justify-center font-mono bg-gradient-to-r from-cyan-500 from 10% via-indigo-500 via 50% to-sky-500 to-100%">
+            <div>
+                <h1 className="text-5xl font-bold align-text-top ">Welcome</h1>
+                <p>Welcome back! Please enter your details.</p>
+                <div>
+                    <label>Email</label>
+                </div>
+
+
+   
+  
+            </div>
+    
+        </section>
+        </>
+
+    )
+}
+export default Login;
